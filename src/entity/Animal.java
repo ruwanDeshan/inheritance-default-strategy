1d3c24cfc783ed06b2e8a9ea8999bba8d42e17fc
ruwanDeshan <117300634+ruwanDeshan@users.noreply.github.com>
2023-05-03 18:55:42 +0530

    commit3 --->nomalization

---
+++ src/entity/Animal.java
@@ -0,0 +1,48 @@
+package entity;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+
+@DiscriminatorValue(value = "animal_data")
+public class Animal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long animalId;
+    private String name;
+    private String genderType;
+
+    public Animal() {
+    }
+
+    public Animal(long animalId, String name, String genderType) {
+        this.animalId = animalId;
+        this.name = name;
+        this.genderType = genderType;
+    }
+
+    public long getAnimalId() {
+        return animalId;
+    }
+
+    public void setAnimalId(long animalId) {
+        this.animalId = animalId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenderType() {
+        return genderType;
+    }
+
+    public void setGenderType(String genderType) {
+        this.genderType = genderType;
+    }
+}
